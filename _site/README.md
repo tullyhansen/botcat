097ef6b9e596b853f22162c8b5ffe89d00947f15
Tully Hansen <tully@tullyhansen.com>
2015-09-22 10:41:49 +1000

    First pages commit

---
+++ _site/README.md
@@ -0,0 +1,13 @@
+# bots
+
+Twitter bot cataloguing project. There are many like it, but this is mine.
+
+Built with [Exhibit][], pulling data from [a Google Sheet][].
+
+## TODO
+
+- Better linking to Twitter profiles using Exhibit's `data-ex-href-subcontent`
+- Figure out the relational (?) database part
+
+[Exhibit]: http://simile-widgets.org/exhibit3/
+[a Google Sheet]: https://docs.google.com/spreadsheets/d/1vleb6Y37ctkjlWbCiTemABgpit83eqW1FT8HEwc0l9I/
---
+++ js/schema.js
@@ -0,0 +1,44 @@
+{
+    "types": {
+        "Item": {
+            "label": "bot-related thing",
+            "pluralLabel": "bot-related things"
+        },
+        "Bot": {
+            "label": "bot",
+            "pluralLabel": "bots"
+        },
+        "Botmaker": {
+            "label": "botmaker",
+            "pluralLabel": "botmakers"
+        }
+    },
+    "properties": {
+        "id": {
+            "valueType": "number"
+        },
+        "url": {
+            "valueType": "url"
+        },
+        "presidency": {
+            "valueType":              "item"
+        },
+        "term": {
+            "valueType":              "number"
+        },
+        "inDate": {
+            "valueType":              "date",
+            "label":                  "in office"
+        },
+        "outDate": {
+            "valueType":              "date",
+            "label":                  "out office"
+        },
+        "birth": {
+            "valueType":              "date"
+        },
+        "death": {
+            "valueType":              "date"
+        }
+    }
+}
