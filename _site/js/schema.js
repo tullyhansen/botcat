766910289b2e64821ff421dcf79392749f78d36b
Tully Hansen <tully@tullyhansen.com>
2015-10-22 15:28:11 +1100

    Let Jekyll do thang

--- _site/js/schema.js
+++ _site/js/schema.js
@@ -4,13 +4,21 @@
             "label": "bot-related thing",
             "pluralLabel": "bot-related things"
         },
-        "Bot": {
+        "bot": {
             "label": "bot",
             "pluralLabel": "bots"
         },
-        "Botmaker": {
-            "label": "botmaker",
-            "pluralLabel": "botmakers"
+        "botally": {
+            "label": "botALLY",
+            "pluralLabel": "botALLIES"
+        },
+        "botmedia": {
+            "label": "work concerning bots",
+            "pluralLabel": "works concerning bots"
+        },
+        "botmaterials": {
+            "label": "bot component",
+            "pluralLabel": "bot components"
         }
     },
     "properties": {
@@ -39,6 +47,37 @@
         },
         "death": {
             "valueType":              "date"
+        },
+        "creators": {
+            "valueType":"botally"
+        },
+        "followers_count": {
+            "valueType":"number",
+            "label": "follower",
+            "pluralLabel": "followers"
+        },
+		"createdBy": {
+        	"label":        "created by",
+            "reverseLabel": "creator of",
+			"valueType":    "item"
+	   },
+	   "citedIn": {
+		   "label":        "cited in",
+		   "reverseLabel": "cites",
+		   "valueType":    "item"
+	   },
+	   "madeWith": {
+		   "label":        "made with",
+		   "reverseLabel": "used to make",
+		   "valueType":    "item"
+	   },
+        "example": { 
+            "label":                  "author", 
+            "pluralLabel":            "authors", 
+            "reverseLabel":           "author of", 
+            "reversePluralLabel":     "authors of", 
+            "groupingLabel":          "their authors", 
+            "reverseGroupingLabel":   "their work" 
         }
     }
 }
